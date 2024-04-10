5a7f286b97fd102009e342399baad358741f5776
Deran Decker <me@derandecker.com>
2024-04-10 12:07:42 -1000

    give MainScreen own file. include "when" answers.

--- app/src/main/java/com/derandecker/colorappv1/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/derandecker/colorappv1/viewmodel/MainViewModel.kt
@@ -10,23 +10,53 @@ class MainViewModel : ViewModel() {
     val backgroundColor = mutableStateOf(Color(0xFFFFFFFF))
 
     fun changeBackgroundColor(chosenColor: ChosenColor) {
-        if (chosenColor == ChosenColor.RED) {
-            backgroundColor.value = Color(0xFFFF0000)
+
+        backgroundColor.value = when (chosenColor) {
+            ChosenColor.RED -> Color(0xFFFF0000)
+            ChosenColor.BLUE -> Color(0xFF0000FF)
+            ChosenColor.GREEN -> Color(0xFF00FF00)
+            ChosenColor.YELLOW -> Color(0xFFFFFF00)
+            ChosenColor.PURPLE -> Color(0xFFA020F0)
+            ChosenColor.WHITE -> Color(0xFFFFFFFF)
         }
-        if (chosenColor == ChosenColor.BLUE) {
-            backgroundColor.value = Color(0xFF0000FF)
-        }
-        if (chosenColor == ChosenColor.GREEN) {
-            backgroundColor.value = Color(0xFF00FF00)
-        }
-        if (chosenColor == ChosenColor.YELLOW) {
-            backgroundColor.value = Color(0xFFFFFF00)
-        }
-        if (chosenColor == ChosenColor.PURPLE) {
-            backgroundColor.value = Color(0xFFA020F0)
-        }
-//        if (chosenColor == ChosenColor.WHITE) {
-//            backgroundColor.value = Color(0xFFFFFFFF)
+
+//
+//        if (chosenColor == ChosenColor.RED) {
+//            backgroundColor.value = Color(0xFFFF0000)
+//        }
+//        if (chosenColor == ChosenColor.BLUE) {
+//            backgroundColor.value = Color(0xFF0000FF)
+//        }
+//        if (chosenColor == ChosenColor.GREEN) {
+//            backgroundColor.value = Color(0xFF00FF00)
+//        }
+//        if (chosenColor == ChosenColor.YELLOW) {
+//            backgroundColor.value = Color(0xFFFFFF00)
+//        }
+//        if (chosenColor == ChosenColor.PURPLE) {
+//            backgroundColor.value = Color(0xFFA020F0)
+//        }
+////        if (chosenColor == ChosenColor.WHITE) {
+////            backgroundColor.value = Color(0xFFFFFFFF)
+////        }
+
+//        when (chosenColor) {
+//            ChosenColor.RED -> backgroundColor.value = Color(0xFFFF0000)
+//            ChosenColor.BLUE -> backgroundColor.value = Color(0xFF0000FF)
+//            ChosenColor.GREEN -> backgroundColor.value = Color(0xFF00FF00)
+//            ChosenColor.YELLOW -> backgroundColor.value = Color(0xFFFFFF00)
+//            ChosenColor.PURPLE -> backgroundColor.value = Color(0xFFA020F0)
+//            ChosenColor.WHITE -> backgroundColor.value = Color(0xFFFFFFFF)
+//        }
+//
+//
+//        backgroundColor.value = when (chosenColor) {
+//            ChosenColor.RED -> Color(0xFFFF0000)
+//            ChosenColor.BLUE -> Color(0xFF0000FF)
+//            ChosenColor.GREEN -> Color(0xFF00FF00)
+//            ChosenColor.YELLOW -> Color(0xFFFFFF00)
+//            ChosenColor.PURPLE -> Color(0xFFA020F0)
+//            ChosenColor.WHITE -> Color(0xFFFFFFFF)
 //        }
     }
 }
